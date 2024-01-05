a90b67937203d4b56ecad61af2378ec9c327b362
Leila Sinore <leilasinore99@gmail.com>
2024-01-05 19:14:51 +0300

    remove duplicate footer

--- src/Layouts/MainLayout.js
+++ src/Layouts/MainLayout.js
@@ -1,22 +1,6 @@
 import React from "react";
-import {
-  Box,
-  Grid,
-} from "@mui/material";
 const MainLayout = ({ children }) => {
-  return (
-    <main>
-      {children}
-      <Grid container spacing={2}>
-        <Grid item sm={12} md={6}></Grid>
-        <Grid item sm={12} md={6}>
-          <Box display="flex" justifyContent="center" alignItems="center">
-            Footer
-          </Box>
-        </Grid>
-      </Grid>
-    </main>
-  );
+  return <main>{children}</main>;
 };
 
 export default MainLayout;
